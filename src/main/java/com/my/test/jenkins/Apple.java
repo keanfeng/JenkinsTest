70f340892999ca1c5afe36382dd8d25f6bebe530
keanfeng <keanfeng@sina.com>
2018-07-22 17:31:41 +0800

    first commit

---
+++ src/main/java/com/my/test/jenkins/Apple.java
@@ -0,0 +1,15 @@
+package com.my.test.jenkins;
+
+public class Apple {
+
+	private String appName = "good app";
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+	
+}
